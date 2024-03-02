71d07018ee1aa23b173ba802fe4900630db82c64
econb <econtrerasb@unal.edu.co>
2024-03-02 10:45:47 -0500

    hola

---
+++ src/main/java/co/edu/unal/laboratoriounal/Mymathec.java
@@ -0,0 +1,19 @@
+public class Mymathec    
+{    
+    public static void main(String[] args)     
+    {    
+        double x = 28;    
+        double y = 4;    
+          
+
+        // return the logarithm of given value       
+        System.out.println("Logarithm of x is: " + Math.log(x));   
+        System.out.println("Logarithm of y is: " + Math.log(y));  
+          
+        // return the logarithm of given value when base is 10      
+        System.out.println("log10 of x is: " + Math.log10(x));   
+        System.out.println("log10 of y is: " + Math.log10(y));    
+
+  
+    }    
+}    
